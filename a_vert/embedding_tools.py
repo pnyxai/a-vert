f49089f6f80f3366d2e0854e55901726c6d269df
Ramiro Rodriguez Colmeiro <rawthil@gmail.com>
2025-08-15 15:13:23 -0300

    fixes on reranker | added TEI rerank | added rerank batching

--- a_vert/embedding_tools.py
+++ a_vert/embedding_tools.py
@@ -72,7 +72,13 @@ def check_and_apply_template(template, placeholder, query):
         if placeholder not in template:
             err = f"Instruction must contain a {placeholder} placeholder"
             raise ValueError(err)
-        return template.format(query=query)
+        if placeholder == "{query}":
+            return template.format(query=placeholder)
+        elif placeholder == "{document}":
+            return template.format(document=placeholder)
+        else:
+            err = f"Placeholder not supported: {placeholder}"
+            raise ValueError(err)
     else:
         return query
 
@@ -100,6 +106,37 @@ def calculate_embedding_distances(model_response,
     return all_distances
 
 
+
+def tei_rerank_call(query, targets, tei_rerank_endpoint):
+    """Calls the TEI endpoint and return the ranking scores as an array, in the 
+    same order as they were provided.
+    """
+    # Get ranks
+    payload = {
+            "query": query,
+            "texts": targets,
+        }
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(tei_rerank_endpoint, data=json.dumps(payload), headers=headers)
+    response = json.loads(response.text)
+
+    total_ranks = len(response)
+    if total_ranks != len(targets):
+        print(query)
+        print(targets)
+        print(response)
+        raise ValueError("Received less scores than targets from endpoint, cannot continue.")
+
+    # Process results and get scores in same order as targets
+    all_scores = np.zeros((total_ranks))
+    for ranked in response:
+        all_scores[ranked['index']] = ranked['score']
+
+    return all_scores
+    
+
+
+
 def vllm_rerank_call(query, targets, vllm_rerank_endpoint, vllm_model_name):
     """Calls the vLLM endpoint and return the ranking scores as an array, in the 
     same order as they were provided.
@@ -115,7 +152,11 @@ def vllm_rerank_call(query, targets, vllm_rerank_endpoint, vllm_model_name):
     response = json.loads(response.text)
 
     total_ranks = len(response['results'])
-    assert total_ranks == len(targets), "Received less scores than targets from endpoint, cannot continue."
+    if total_ranks != len(targets):
+        print(query)
+        print(targets)
+        print(response)
+        raise ValueError("Received less scores than targets from endpoint, cannot continue.")
 
     # Process results and get scores in same order as targets
     all_scores = np.zeros((total_ranks))
@@ -125,6 +166,40 @@ def vllm_rerank_call(query, targets, vllm_rerank_endpoint, vllm_model_name):
     return all_scores
     
 
+def get_rerank(query, targets, endpoint, endpoint_type, model_name=None, max_batch_size=32):
+    """Call the .0.. batch 
+    size.
+    """
+    # Assign endpoint call
+    if endpoint_type == "tei":
+        reranking_call = lambda x,y: tei_rerank_call(x, y, endpoint)
+    elif endpoint_type == "vllm" or endpoint_type == "openai":
+        if model_name is None:
+            raise ValueError("Model name is required for vllm/openai endpoint.")
+        reranking_call = lambda x,y: vllm_rerank_call(x, y, endpoint, model_name)
+    else:
+        raise ValueError("Endpoint type not supported")
+
+    # Discount query place
+    max_batch_size -= 1 
+    # Calculate embeddings for the text list
+    if isinstance(targets, list):
+        if len(targets) > max_batch_size:
+            partial = reranking_call(query, targets[:max_batch_size])
+            last = max_batch_size
+            while last < len(targets):
+                if last+max_batch_size > len(targets):
+                    part = reranking_call(query, targets[last:])
+                else:
+                    part = reranking_call(query, targets[last:last+max_batch_size])
+                last += max_batch_size
+                partial = np.concatenate([partial, part], axis=0)
+            return partial
+                
+        else:
+            return reranking_call(query, targets)
+    else:
+        return reranking_call(query, targets)
 
 def calculate_reranking_distances(model_response,
                                   batch,
@@ -133,20 +208,15 @@ def calculate_reranking_distances(model_response,
                                   model_name=None,
                                   query_template=None,
                                   document_template=None,
+                                  batch_size=32
                                   ):
-    # Assign endpoint call
-    if endpoint_type == "vllm" or endpoint_type == "openai":
-        if model_name is None:
-            raise ValueError("Model name is required for vllm/openai endpoint.")
-        reranking_call = lambda x,y: vllm_rerank_call(x, y, endpoint, model_name)
-    else:
-        raise ValueError("Endpoint type not supported")
-
-    # Apply instruction and template if selected
+    
+    # Calculate targets embeddings
     batch_to_rank = [check_and_apply_template(document_template, "{document}", t) for t in batch]
     model_response_to_rank = check_and_apply_template(query_template, "{query}", model_response)
 
-    # Calculate targets raking scores
-    all_scores = reranking_call(model_response_to_rank, batch_to_rank)
+    
+    # Get model response rerank
+    all_scores = get_rerank(model_response_to_rank, batch_to_rank, endpoint, endpoint_type, model_name=model_name, max_batch_size=batch_size)
 
     return all_scores
