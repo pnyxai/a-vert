796951941c1db898eb9b1fb8a25bdda846c53b4d
Ramiro Rodriguez Colmeiro <rawthil@gmail.com>
2026-05-01 17:07:42 -0300

    multiple fixes to tasks for edge cases

--- lm-eval_tasks/gpqa_subtask/chat_zero-shot_a-vert/utils.py
+++ lm-eval_tasks/gpqa_subtask/chat_zero-shot_a-vert/utils.py
@@ -60,7 +60,7 @@ def doc_eval(pred, refs, question, choices, task):
 
     # ----------------------- A-VERT -------------------------------------------
     none_answer_placeholder = os.environ.get("LMEVAL_MODEL_NONE_ANSWER_PLACEHOLDER")
-    if len(pred.strip()) == 0 or pred == none_answer_placeholder:
+    if pred is None or len(pred.strip()) == 0 or pred == none_answer_placeholder:
         # This is not a valid generation
         a_vert_match = False
         a_vert_correct_score = 0.0
